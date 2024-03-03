8c94c9a168906dcc976c1db1c67227978c042776
stilllove1511 <100393523+stilllove1511@users.noreply.github.com>
2024-03-03 14:41:37 +0000

    Update server port to 3001

--- src/server.ts
+++ src/server.ts
@@ -14,6 +14,6 @@ app.post('/ask', async (req, res) => {
     })
 })
 
-app.listen(3000, () => {
-    console.log('Server is running on port 3000')
+app.listen(3001, () => {
+    console.log('Server is running')
 })
